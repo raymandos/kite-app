d3c112b677c7d89b498a83d34e24e898463908d2
Raymando <alungu@hotmail.com>
2022-04-07 16:37:10 +0300

    Added additional resources for icons

---
+++ src/Resources/iconMarkerYellow.js
@@ -0,0 +1,12 @@
+import L from 'leaflet'
+import marker from '../Resources/svg/map-marker-yellow.svg'
+
+const iconMarkerYellow = new L.Icon({
+    iconUrl: marker,
+    iconRetinaUrl: marker,
+    iconAnchor: [10, 60],
+    popupAnchor: [10, -44],
+    iconSize: [40, 40],
+})
+
+export { iconMarkerYellow }
---
+++ src/Resources/listColumns.js
@@ -0,0 +1,28 @@
+const columns = [
+  {
+    label: 'Name',
+    field: 'name'
+  },
+  {
+    label: 'Country',
+    field: 'country'
+  },
+  {
+    label: 'Latitude',
+    field: 'lat'
+  },
+  {
+    label: 'Longitude',
+    field: 'long'
+  },
+  {
+    label: 'Wind prob.',
+    field: 'probability'
+  },
+  {
+    label: 'When to go',
+    field: 'month'
+  }
+]
+
+export { columns }
